a7510fb91905157c89d7618780412fafb5114fce
thinkpad <natachak2007@gmail.com>
2018-09-19 15:21:22 +0300

    first try

--- src/Fibonachi/Fibonacci.java
+++ src/Fibonachi/Fibonacci.java
@@ -1,9 +1,9 @@
 package Fibonachi;
 class Fibonacci {
 
-    static int n1=1,n2=1,n3=0;
+    static int n1=0,n2=1,n3=0;
 static void printFibonacci(int count){
-        if(count>-1){
+        if(count>0){
         n3 = n1 + n2;
         n1 = n2;
         n2 = n3;
@@ -12,7 +12,8 @@ static void printFibonacci(int count){
         }
         }
     public static void main(String args[]) {
-    int count = 6;
+    int count = 7
+            ;
     System.out.print(n1 + " " + n2);
     printFibonacci(count - 2);
 
